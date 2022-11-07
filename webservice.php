78dc61a3028230d89b63fde547cfbc3a2d99d354
welead <30601062+welead@users.noreply.github.com>
2022-11-07 15:01:44 +0100

    Update webservice.php

--- webservice.php
+++ webservice.php
@@ -1,1 +1,26 @@
+<?php
 
+    header('Access-Control-Allow-Origin: *');  
+    header('Content-Type: text/html; charset=utf-8');
+	  ini_set ("display_errors",1);
+
+
+  if ($_REQUEST['fct']=="UserLogin") 
+  {
+      // tester si la correspondance email / pass est bonne + renvoyer un json avec le résultat 
+  }
+
+  if ($_REQUEST['fct']=="UserAdd") 
+  {
+    // Ajouter l'utilisateur a la db, controle des champs + sécurité des requetes
+  }
+
+  if ($_REQUEST['fct']=="UserPasswordReset") 
+  {
+    
+    
+  }
+
+
+
+?>
